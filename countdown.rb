69850c3b38c9a2415ba6873250432c9557d166fd
RokhayaN <rokhayamuslima1@gmail.com>
2019-10-10 22:02:48 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
 #write your code here
 
-def countdown
+def countdown (5)
+  number = 0
+  while number <= 5
+  puts "#{number} then "
+  number+= 1
+  When number == 5
+  puts "HAPPY NEW YEAR"
 end
+
